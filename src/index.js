61b239da777294ff0f081b9880e9724efb286edf
SvenWehde <wolf-thriller@gmx.de>
2025-06-27 21:03:33 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+javascriptimport React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SonnensucheApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SonnensucheApp />
+  </React.StrictMode>
+);
