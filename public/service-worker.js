d45f7acd56cad2f7b71fa87917ed09b6a8da714d
SvenWehde <wolf-thriller@gmx.de>
2025-09-25 17:35:44 +0200

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'sonnensuche-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
